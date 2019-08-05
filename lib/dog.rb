b4e61acda812315e46d9e2b59efc8ff02b1dd441
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-08-05 06:31:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,25 +1,20 @@
 class Dog
-  def initialize (name, breed)
-    @name= name
+  def initialize (name, breed = "Mutt")
+    @name = name
+    @breed = breed
   end
-  def name= (name)
-    @name= name
+
+  def name=(name, breed = "Mutt")
+    @name = name
+    @breed = breed
   end
- def name
-   @name
- end
- def breed(breed)
-   @breed= breed
- end
- def breed= (breed)
-  @breed= breed
- end
- def breed
-   @breed
- end
- end
-     def breed (breed)
-      puts #{breed}"
-    end
+
+  def name
+    @name
+  end
+  def breed
+    @breed
+  end 
+end 
  
  
